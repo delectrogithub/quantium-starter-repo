666b9b850fd2b22e1a589aeec3bedb9441e899c6
delectrogit <101830209+delectrogithub@users.noreply.github.com>
2022-12-26 14:50:22 +0000

    Revert "Revert "I committed the files using PyCharm""
    This reverts commit 7a730af4b8d61805c62a53b085000232328a3a86.

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 import csv
+import os
 
-def output_formatted_line(filename):
- with open(filename) as csv_file:
-     csv_reader = csv.reader(csv_file, delimiter=',')
-     line_count = 0
-     for row in csv_reader:
-         if line_count == 0:
-             print(f'Column names are {", ".join(row)}')
-             line_count += 1
-         else:
-             print(f'\tThere has been {row[2]} sales on {row[3]} in the {row[4]} region .')
-             line_count += 1
+datardir = "./data"
+output = "./data.csv"
 
-database = int(input('Please enter the databse you would like to  reach 0, 1 or 2: '))
+# open the output file
+with open(output, "w") as output_file:
+    header = ["sales", "date", "region"]
+    csv.writer(output_file).writerow(header)
+    for file_name in os.listdir(datardir):
+        with open(f"{datardir}/{file_name}", "r") as input_file:
+            reader = csv.reader(input_file)
+            row_index = 0
+            for input_row in reader:
+                if row_index > 0:
+                    product = input_row[0]
+                    raw_price = input_row[1]
+                    quantity = input_row[2]
+                    date = input_row[3]
+                    region = input_row[4]
 
-if database == 0:
-    output_formatted_line('data/daily_sales_data_0.csv')
-elif database == 1:
-    output_formatted_line('data/daily_sales_data_1.csv')
-elif database == 2:
-    output_formatted_line('data/daily_sales_data_2.csv')
-else:
-    print('That is not a valid database, please retry.')
+                    if product == "pink morsel":
+                        sale = float(raw_price[1:]) * int(quantity)
+                        output_row = [sale, date, region]
+                        csv.writer(output_file).writerow(output_row)
+                row_index += 1
